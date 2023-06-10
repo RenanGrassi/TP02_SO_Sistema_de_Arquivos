3d33296dedfbe9732c474e8c267c3087d57d1f94
Renan <renan.procopio@ufv.br>
2023-06-10 00:09:42 -0300

    Correção do iNode

--- inode.h
+++ inode.h
@@ -1,7 +1,42 @@
+#include <stdio.h>
+#include <stdbool.h>
 #include <time.h>
+#include <string.h>
 
+#define MAX_FILENAME_SIZE 256
+#define MAX_BLOCK_POINTERS 10
+
+typedef struct {
+    char filename[MAX_FILENAME_SIZE]; // Nome do arquivo com tamanho fixo
+    int permissions; // Permissões do arquivo são um inteiro que será tratado como binário de acordo com o padrão UNIX
+    int size; // Tamanho em bytes
+    bool is_directory; // Indica se é um diretório ou não
+    time_t created_at; // Criado em...
+    time_t modified_at; // Modificado em...
+    time_t last_accessed_at; // Acessado em...
+    int data_blocks[MAX_BLOCK_POINTERS];
+    int indirect_block; // Bloco indireto para referenciar mais blocos de dados, se necessário
+}INode;
+
+/*
+    O campo indirect_block permite que um i-node referencie um bloco indireto, que por sua vez contém mais referências 
+    a blocos de dados. Essa abordagem indireta é usada para suportar arquivos maiores que o número máximo de blocos 
+    diretamente referenciados pelos data_blocks. Faltam outras estruturas de dados, como 
+    estruturas de diretório, uma tabela de i-nodes, um mapa de bits para rastrear blocos livres, além de funções 
+    para manipulação de arquivos e diretórios.
+*/
+
+// Função para inicializar um Inode
+void createRootDirectory(INode* inode, char* filename);
+
+void createDirectory(INode* inode, char* filename);
+
+void renameDirectory(INode* inode, char* filename);
+
+void createFile(INode* inode, char* filename);
+
+/*#include <time.h>
 #define MAX_BLOCKS 10 // Número máximo de blocos de dados
-
 typedef struct data_block * dtb;
 
 typedef struct {
@@ -30,4 +65,4 @@ typedef struct block{
 
 
 gerenciador de partiçao tem lista 10.000
-partição[0] = inode os endereços do dado (3 data_blocks)
+partição[0] = inode os endereços do dado (3 data_blocks)*/
